432ac3da10fa968107b46378f48ba1b8ee5b2176
Izaac-eduardo <izaaceduardosoares24@gmail.com>
2025-06-09 19:59:21 -0300

    Commitando com o Douglas

---
+++ scrip.js
@@ -0,0 +1,6 @@
+let processador = document.querySelector("#processador");
+let memoria = document.querySelector("#mr");
+let res = document.getElementById("resultado");
+if (processador === null || memoria === null || res === null) {
+  console.error("Elementos não encontrados no DOM.");
+} 
